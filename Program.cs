7c63d85aebb70d5970a9db25e9ec090dc8b0fa49
Howard.YH.Hung <howard.yh.hung@fii-foxconn.com>
2025-09-15 14:09:10 +0800

    feat: middle ware sequence

--- Program.cs
+++ Program.cs
@@ -62,13 +62,14 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+
 }
 app.UseMiddleware<ExceptionHandlingMiddleware>();
-app.UseRequestLocalization();
+// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+app.UseHsts();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+app.UseRequestLocalization();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
